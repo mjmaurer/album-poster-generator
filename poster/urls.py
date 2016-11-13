9d61e2ecaefc7467ee6dafee966de94a2aaf98ff
Michael Maurer <mjmaurer7@gatech.edu>
2016-11-13 17:34:06 -0500

    .

--- poster/urls.py
+++ poster/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^spotify_auth$', views.spotify_auth, name='spotify_auth'),
     url(r'^spotify_setup$', views.spotify_setup, name='spotify_setup'),
+    url(r'^spotify_main$', views.spotify_main, name='spotify_main'),
+    url(r'^lastfm_setup$', views.spotify_setup, name='lastfm_setup'),
 ]
